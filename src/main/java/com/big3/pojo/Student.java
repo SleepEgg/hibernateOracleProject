000fc9e24da2d04a536476772ead02bbfb0a3b52
SleepEgg <44051262+SleepEgg@users.noreply.github.com>
2018-11-27 16:21:44 +0800

    Add files via upload

---
+++ src/main/java/com/big3/pojo/Student.java
@@ -0,0 +1,65 @@
+package com.big3.pojo;
+
+import javax.persistence.Basic;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+@Entity
+public class Student {
+    private long id;
+    private String name;
+    private Long age;
+
+    @Id
+    @Column(name = "ID")
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    @Basic
+    @Column(name = "NAME")
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Basic
+    @Column(name = "AGE")
+    public Long getAge() {
+        return age;
+    }
+
+    public void setAge(Long age) {
+        this.age = age;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Student student = (Student) o;
+
+        if (id != student.id) return false;
+        if (name != null ? !name.equals(student.name) : student.name != null) return false;
+        if (age != null ? !age.equals(student.age) : student.age != null) return false;
+
+        return true;
+    }
+
+    @Override
+    public int hashCode() {
+        int result = (int) (id ^ (id >>> 32));
+        result = 31 * result + (name != null ? name.hashCode() : 0);
+        result = 31 * result + (age != null ? age.hashCode() : 0);
+        return result;
+    }
+}
---
+++ src/test/hiberoracle.java
@@ -0,0 +1,72 @@
+import com.big3.pojo.Student;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
+import org.hibernate.cfg.Configuration;
+import org.hibernate.query.Query;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+
+
+public class hiberoracle {
+    private SessionFactory sessionFactory;
+    private Session session;
+    private Transaction transaction;
+
+    @Before
+    public void before(){
+        Configuration configuration = new Configuration().configure();//创建配置文件
+        sessionFactory = configuration.buildSessionFactory();//创建会话工厂
+        session = sessionFactory.openSession();//开启会话
+        transaction = session.beginTransaction();//开启事物
+    }
+
+    @After
+    public void after(){
+        transaction.commit();//提交
+        session.close();//关闭会话
+        sessionFactory.close();//关闭会话工厂
+    }
+
+    @org.junit.Test
+    public void testUpdate(){
+        Query query = session.createQuery("from Student ");
+        List<Student> studentList = query.list();
+        Student student = studentList.get(6);
+        student.setName("Mona");
+       }
+
+    @Test
+    public void testCreate(){
+        Query query = session.createQuery("from Student ");
+        Student studentnew = new Student();
+        studentnew.setName("Mona");
+        studentnew.setId(8l);
+        studentnew.setAge(1l);
+        session.save(studentnew);
+    }
+
+    @Test
+    public void testDelete(){
+        Query query = session.createQuery("from Student ");
+        Student student = new Student();
+        student.setId(8);
+        session.delete(student);
+    }
+
+    @Test
+    public void testSelect(){
+        Query query = session.createQuery("select id,name,age from Student ");
+        List<Object[]> list = query.list();
+        for(Object[] stu :list){
+            System.out.println("id:"+stu[0]);
+            System.out.println("name:"+stu[1]);
+            System.out.println("age:"+stu[2]);
+        }
+    }
+}
+
